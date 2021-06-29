a1e834573182214162eb07060a5e07f3f70a7837
Mukesh <crkumarmukesh@gmail.com>
2021-06-29 20:59:58 +0530

    Added code for How to handle elements using JavaScriptExecutor

--- src/test/java/Basics/JavaScriptExecutor.java
+++ src/test/java/Basics/JavaScriptExecutor.java
@@ -1,33 +1,35 @@
 package Basics;
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import Utiltiy.JavaExecutor_Methods;
-import Utiltiy.Highlight_Elements;
 
-public class Highlight_element {
+public class JavaScriptExecutor {
 
 	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\mukesh\\eclipse-workspace\\Demo_Selenium\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-
-		// WebDriver driver = new HtmlUnitDriver();
 		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		System.out.println(driver.getTitle());
 		WebElement loginbutton = driver.findElement(By.xpath("//input[@id='btnLogin']"));
 
-		// class coming from utility package for re-use purpose
-		JavaExecutor_Methods.flash(loginbutton, driver);
+		JavaExecutor_Methods.flash(loginbutton, driver);// highlighting the element
+		JavaExecutor_Methods.drawBorder(loginbutton, driver); // border for element
+		JavaExecutor_Methods.generateAlert(driver, "Username and password cannot be Empty");//generating an alert
+		JavaExecutor_Methods.clickElementByJS(loginbutton, driver);//clicking element by JS
+		JavaExecutor_Methods.refreshBrowserByJS(driver); // refreshing the page by JS
+		JavaExecutor_Methods.getPageInnerText(driver); // to get page inner text
+		JavaExecutor_Methods.getTitleByJS(driver); // get title
+		JavaExecutor_Methods.scrollIntoView(loginbutton, driver); //scroll in to view
+		JavaExecutor_Methods.scrollPageDown(driver); // page scroll down
 	}
-
 }
---
+++ src/test/java/Utiltiy/DrawborderforElement.java
@@ -0,0 +1,15 @@
+package Utiltiy;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DrawborderforElement {
+
+	public static void drawBorder(WebElement element, WebDriver driver) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].style.border='3px solid rd'", element);
+
+	}
+
+}
